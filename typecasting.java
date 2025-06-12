83bc4bbdbc1d7762b50aaee514cfc1c67a0dc325
an-sh007 <102963297+an-sh007@users.noreply.github.com>
2025-06-12 12:51:39 +0530

    cw

--- typecasting.java
+++ typecasting.java
@@ -7,10 +7,13 @@ public class typecasting {
         double d= i;
         double u = 11.8;
         int r=(int)u;
+        int n =10;
+        char c = (char)n ;
 
         System.out.println("the long value is  " +l );
-         System.out.println("the long value is  " +f );
-            System.out.println("the long value is  " +d);
-             System.out.println("the long value is  " +r);
+         System.out.println("the float value is  " +f );
+            System.out.println("the double value is  " +d);
+             System.out.println("the narrowing value is  " +r);
+               System.out.println("the char value is  " +c);
     }
 }
