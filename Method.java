eea11fc26b958414aa7b9c73cc1192a6acc58a34
an-sh007 <102963297+an-sh007@users.noreply.github.com>
2025-06-19 12:50:24 +0530

    java

---
+++ Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+        System.out.println("hi ");
+        test();
+    }
+   public static void test()
+    {
+System.out.println("heyyy");
+    }
+    
+}
