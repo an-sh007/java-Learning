31d4a9776c2e078d9593553069477c748cb11b6d
an-sh007 <102963297+an-sh007@users.noreply.github.com>
2025-06-19 12:57:16 +0530

    nw

--- Method.java
+++ Method.java
@@ -2,10 +2,17 @@ public class Method {
     public static void main(String[] args) {
         System.out.println("hi ");
         test();
+        test1();
     }
    public static void test()
     {
 System.out.println("heyyy");
+    }
+    public static void test1()
+    {
+        int a=10;
+        int b=19;
+        System.out.println("the sssum is   "+(a+b));
     }
     
 }
