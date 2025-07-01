d36af26558d206bc7c447c35266be15c2ddaae30
an-sh007 <102963297+an-sh007@users.noreply.github.com>
2025-07-01 12:53:52 +0530

    java

---
+++ dowhile.java
@@ -0,0 +1,15 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i=20;
+        do {
+            System.out.println("The value of i is: " + i);
+            i=i-2;
+        } while (i>=0);
+        // while (i<=6) {
+        //     System.out.println("The value of i is: " + i);
+        //     i=i+2;
+            
+        // }
+    }
+
+}
